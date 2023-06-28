6dd16a107f635c4eaa09b1f8c981186061c9476d
ilia morozov <ilia.m.composer@gmail.com>
2023-06-28 15:47:21 +0100

    made kpis dynamic

---
+++ src/dashboard/assets/js/api/get-token-test.js
@@ -0,0 +1,32 @@
+
+
+// var setup = $.ajaxSetup({
+//     url: 'https://europe.grc1.com/iGRC/Auth/token',
+//     method: 'POST',
+//     data: {
+//         username: "esgapiadmin",
+//         password: "JGzAunUj&K81",
+//         grant_type: "password"
+//     },
+//   });
+
+// setTimeout(() => {
+//     $.ajax({
+//         url: "https://data.esg-one.uk/esg1-api/api/GetClientbyId?Id=1",
+//         method: "GET",
+//         headers: {
+//             Authorization: 'Bearer ' + token,
+//             "Content-Type": "application/json"
+//         },
+//         params: {
+//             id: 1
+//         },
+//         success: function (data2) {
+//             console.log('success', data2)
+//         },
+//         error: (e) => {
+//             console.log('error', e)
+//         }
+//     });
+// }, 2000)
+
